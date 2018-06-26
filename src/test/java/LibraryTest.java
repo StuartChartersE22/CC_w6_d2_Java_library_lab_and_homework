f53d66bb9f4a3d83ab6d2aeddde4456df684c081
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-26 15:35:09 +0100

    libray has no books when starting

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,9 +1,27 @@
-//public class LibraryTest {
-//
-//    private Library library;
-//    private Book book;
-//
-////    @Before
-//
-//
-//}
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book("Five Children and It");
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.bookCount());
+    }
+
+//    @Test
+//    public void libraryContainsBooks(){
+//        assertEquals(1,library.bookCount())
+//    }
+
+}
